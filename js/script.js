bf517a42f46b08176feb3c4d411399614d41aee8
Riya <riyadevelops01@gmail.com>
2024-05-06 15:38:56 -0400

    exit hmaburger menu when tab is clicked

--- js/script.js
+++ js/script.js
@@ -1,13 +1,15 @@
-
 // hamburger variable, hamburger variable selects css element with class name menu
 // JavaScript code creates an event listener for the click event on the hamburger menu icon and toggles the "navbar-open" class on the mobile menu
 document.addEventListener('DOMContentLoaded', function () {
     const hamburger = document.querySelector(".menu");
     const mobileMenu = document.querySelector(".mobile");
     const hamburgerIcon = document.querySelector(".menu i");
+    const navLinks = document.querySelectorAll(".nav__link");
 
+    // Toggle mobile menu and hamburger icon
     hamburger.addEventListener('click', () => {
         mobileMenu.classList.toggle("navbar-open");
+
         // Toggle the hamburger icon between bars and x
         if (mobileMenu.classList.contains("navbar-open")) {
             hamburgerIcon.classList.remove("fa-bars");
@@ -17,7 +19,35 @@ document.addEventListener('DOMContentLoaded', function () {
             hamburgerIcon.classList.add("fa-bars");
         }
     });
+
+    // Close mobile menu when a navigation link is clicked
+    navLinks.forEach(link => {
+        link.addEventListener("click", () => {
+            mobileMenu.classList.remove("navbar-open");
+            hamburgerIcon.classList.remove("fa-times");
+            hamburgerIcon.classList.add("fa-bars");
+        });
+    });
 });
+
+
+
+
+// document.addEventListener('DOMContentLoaded', function () {
+    
+//     const navLink = document.querySelector(".nav__link");
+//     const mobileMenu = document.querySelector(".mobile");
+
+
+// navLink.addEventListener('click', () => {
+//     mobileMenu.classList.add("navbar-close");
+//     hamburgerIcon.classList.remove("fa-times");
+
+
+// });
+
+
+// });
 
 
 // Back to Top Button 
